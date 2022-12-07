28b96111564a369c61775a81e7a162bbbe6935f0
OgembaJr <emmanuel.ogemba@student.moringaschool.com>
2022-12-08 02:14:17 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (e) => {
+  e.preventDefault();
+  
+  const myForm = document.getElementById("create-task-form");
+  myForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const tasksNew = document.getElementById("new-task-description");
+    const taskVar = document.getElementById("tasks");
+    const newTaskList = document.createElement("li");
+    newTaskList.innerText = tasksNew.value;
+    taskVar.appendChild(newTaskList);
+    e.target.reset();
+
+  });
+
 });
